7e040d3d69384ca5c4fbda059964996b6c5ca6b6
davidferro <david-ferro@sbcglobal.net>
2019-02-18 21:05:20 -0500

    fixed a travis issue

--- models/location.js
+++ models/location.js
@@ -29,7 +29,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       validate: {
         isAlpha: true,
-        len: [2,2]
+        len: [2, 2]
       }
     },
     zipcode: {
