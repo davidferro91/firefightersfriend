85ff79230221db5115891845a1f47e61289a4c39
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-02-19 20:59:44 -0500

    added handlebars and routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,55 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+  app.get("/", function (req, res) {
+    res.render("index", {
     });
   });
 
+  // Load home page
+  app.get("/profile", function (req, res) {
+    res.render("profile", {
+    });
+  });
+
+  // Load index page
+  app.get("/equipment", function (req, res) {
+    res.render("equipment", {
+    });
+  });
+
+  // Load index page
+  app.get("/edit-equipment", function (req, res) {
+    res.render("edit-equipment", {
+    });
+  });
+
+  // Load index page
+  app.get("/edit-crew", function (req, res) {
+    res.render("edit-crew", {
+    });
+  });
+  // Load index page
+  app.get("/edit-certs", function (req, res) {
+    res.render("edit-certs", {
+    });
+  });
+
+  // Load index page
+  app.get("/crew", function (req, res) {
+    res.render("crew", {
+    });
+  });
+
+  // Load index page
+  app.get("/home", function (req, res) {
+    res.render("home", {
+    });
+  });
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,7 +57,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
