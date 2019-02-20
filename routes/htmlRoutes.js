4ee83fd4ae6c31a2ee5761766150c08e50982940
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-02-19 21:18:56 -0500

    Resolved Travis issues.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,55 +1,47 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
-    res.render("index", {
-    });
+  app.get("/", function(req, res) {
+    res.render("index", {});
   });
 
   // Load home page
-  app.get("/profile", function (req, res) {
-    res.render("profile", {
-    });
+  app.get("/profile", function(req, res) {
+    res.render("profile", {});
   });
 
   // Load index page
-  app.get("/equipment", function (req, res) {
-    res.render("equipment", {
-    });
+  app.get("/equipment", function(req, res) {
+    res.render("equipment", {});
   });
 
   // Load index page
-  app.get("/edit-equipment", function (req, res) {
-    res.render("edit-equipment", {
-    });
+  app.get("/edit-equipment", function(req, res) {
+    res.render("edit-equipment", {});
   });
 
   // Load index page
-  app.get("/edit-crew", function (req, res) {
-    res.render("edit-crew", {
-    });
+  app.get("/edit-crew", function(req, res) {
+    res.render("edit-crew", {});
   });
   // Load index page
-  app.get("/edit-certs", function (req, res) {
-    res.render("edit-certs", {
-    });
+  app.get("/edit-certs", function(req, res) {
+    res.render("edit-certs", {});
   });
 
   // Load index page
-  app.get("/crew", function (req, res) {
-    res.render("crew", {
-    });
+  app.get("/crew", function(req, res) {
+    res.render("crew", {});
   });
 
   // Load index page
-  app.get("/home", function (req, res) {
-    res.render("home", {
-    });
+  app.get("/home", function(req, res) {
+    res.render("home", {});
   });
   // Load example page and pass in an example by id
   app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -57,7 +49,7 @@ module.exports = function (app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
